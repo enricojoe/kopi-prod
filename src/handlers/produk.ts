962f7f0424ee542f389dcb959bdf9e5a887bf3b9
Kevin Tanuwijaya <kevin.119140207@student.itera.ac.id>
2023-05-22 13:24:19 +0700

    Revert "Schema Update"
    This reverts commit cdc80d4e71fc3c2b06e233b567b902e870d85923.

--- src/handlers/produk.ts
+++ src/handlers/produk.ts
@@ -125,7 +125,6 @@ export const getProductById = async (req, res, next) => {
 			include: {
 				user: {
 					select: {
-						id: true,
 						namaLengkap: true,
 						alamat: true,
 						gambar:true,
